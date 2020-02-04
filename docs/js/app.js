4d91e03448ab01eff658512cf1803b859e26da0b
LarsBreuren <larsbreuren@gmail.com>
2020-02-04 14:58:58 +0100

    Loop for API object added

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,32 +1,51 @@
-fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/60e53b3010cd5224c2ec5d685d4b320b/52.2050221,4.880913?units=si`)
+const mijdrecht = "52.2050221,4.880913";
+const amsterdam = "52.3702157,4.8951679";
+
+var locaties = {
+  mijdrecht: "52.2050221,4.880913",
+  amsterdam: "52.3702157,4.8951679",
+  }
+  console.log(locaties);
+
+
+endpoint = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/',
+    key =  "60e53b3010cd5224c2ec5d685d4b320b/",
+    locatie =(amsterdam),
+    end = "?units=si",
+    url = (endpoint) + (key) + (locatie) + (end);
+
+    console.log(url);
+
+   fetch(url)
   .then((response) => {
     return response.json();
   })
   
   .then((myJson) => {
-    // console.log(myJson);
-    // var para = document.createElement("p");
-    // var node = document.createTextNode(myJson.currently.temperature + " Graden");
-
-    // var para2 = document.createElement("b");
-    // var node2 = document.createTextNode(myJson.currently.humidity + " Vochtigheid");
-
-    // para.appendChild(node);
-    // para2.appendChild(node2);
-
-    // var element = document.getElementById("article");
-    // element.appendChild(para);
-    // element.appendChild(para2);
 
     var obj = {
       temp: myJson.currently.temperature,
       humidity: myJson.currently.humidity,
       cloud: myJson.currently.cloudCover}
 
-      var data = JSON.stringify(obj); 
-      document.getElementById("p").innerHTML = data; 
+      var article = document.getElementById("article");
+
+    for(let key in obj){
+      var postfix;
   
-      console.log(obj)
+      if(key == "temp")
+          postfix = "Graden";
+      else if(key == "humidity")
+          postfix = "Vochtigheid";
+      else
+          postfix = "Bewolking";
+  
+      var p = document.createElement("p");
+      var textNode = document.createTextNode(obj[key] + " " + postfix);
+  
+      p.appendChild(textNode);
+      article.appendChild(p);
+  }
   
   });
 
