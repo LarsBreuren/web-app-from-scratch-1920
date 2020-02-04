18ab91ce78841967c305fb4b0c46fd164dad0135
LarsBreuren <larsbreuren@gmail.com>
2020-02-04 15:51:21 +0100

    Loops fixed

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,54 +1,45 @@
-const mijdrecht = "52.2050221,4.880913";
-const amsterdam = "52.3702157,4.8951679";
-
-var locaties = {
+const locaties = {
   mijdrecht: "52.2050221,4.880913",
   amsterdam: "52.3702157,4.8951679",
   }
-  console.log(locaties);
 
+  function buildUrl(locatie, cor){
+    let endpoint = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'
+    let key =  "60e53b3010cd5224c2ec5d685d4b320b/"
+    let end = "?units=si"
+    return endpoint + key + locatie + end
+  }    
 
-endpoint = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/',
-    key =  "60e53b3010cd5224c2ec5d685d4b320b/",
-    locatie =(amsterdam),
-    end = "?units=si",
-    url = (endpoint) + (key) + (locatie) + (end);
+  for (locatie in locaties) {
+    if (locaties.hasOwnProperty(locatie)) {
+      buildUrl(locatie, locaties[locatie])
+  }
+    
+  }
 
-    console.log(url);
-
-   fetch(url)
+   fetch(buildUrl(locaties.mijdrecht))
   .then((response) => {
     return response.json();
   })
   
   .then((myJson) => {
-
     var obj = {
-      temp: myJson.currently.temperature,
-      humidity: myJson.currently.humidity,
-      cloud: myJson.currently.cloudCover}
+      Temperatuur: myJson.currently.temperature,
+      Vochtigheid: myJson.currently.humidity,
+      Wolken: myJson.currently.cloudCover}
 
       var article = document.getElementById("article");
 
-    for(let key in obj){
-      var postfix;
-  
-      if(key == "temp")
-          postfix = "Graden";
-      else if(key == "humidity")
-          postfix = "Vochtigheid";
-      else
-          postfix = "Bewolking";
-  
+      for (let [key, value] of Object.entries(obj)) {
+        console.log(`${key}: ${value}`);
+
       var p = document.createElement("p");
-      var textNode = document.createTextNode(obj[key] + " " + postfix);
+      var textNode = document.createTextNode(`${key}: ${value}`);
   
       p.appendChild(textNode);
       article.appendChild(p);
-  }
-  
+      }
   });
-
 
   // function getLocation(){
   //   return new Promise((resolve,reject) => {
