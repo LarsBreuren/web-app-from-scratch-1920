c1be645933ef34328234e6c607bacaba104fe274
LarsBreuren <larsbreuren@gmail.com>
2020-02-10 13:49:01 +0100

    Function calc added

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,7 +3,7 @@ const locaties = {
   amsterdam: "52.3702157,4.8951679",
   }
 
-  function buildUrl(locatie, cor){
+  function buildUrl(locatie){
     let endpoint = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'
     let key =  "60e53b3010cd5224c2ec5d685d4b320b/"
     let end = "?units=si"
@@ -13,8 +13,7 @@ const locaties = {
   for (locatie in locaties) {
     if (locaties.hasOwnProperty(locatie)) {
       buildUrl(locatie, locaties[locatie])
-  }
-    
+   }
   }
 
    fetch(buildUrl(locaties.mijdrecht))
@@ -23,35 +22,40 @@ const locaties = {
   })
   
   .then((myJson) => {
-    var obj = {
+    let obj = {
       Temperatuur: myJson.currently.temperature,
       Vochtigheid: myJson.currently.humidity,
-      Wolken: myJson.currently.cloudCover}
+      Wolken: myJson.currently.cloudCover,
+      windSpeed: myJson.currently.windSpeed}
 
-      var article = document.getElementById("article");
+      let temp = obj.Temperatuur;
+      let article = document.getElementById("article");
 
       for (let [key, value] of Object.entries(obj)) {
         console.log(`${key}: ${value}`);
 
-      var p = document.createElement("p");
-      var textNode = document.createTextNode(`${key}: ${value}`);
+      let p = document.createElement("p");
+      let textNode = document.createTextNode(`${key}: ${value}`);
   
       p.appendChild(textNode);
       article.appendChild(p);
       }
-  });
+      rating(temp);
+  });   
 
-  // function getLocation(){
-  //   return new Promise((resolve,reject) => {
-  //     var
-  //       pos
-  //     navigator.geolocation.getCurrentPosition(function(position) {
-  //       pos = {
-  //         lat: position.coords.latitude,
-  //         long: position.coords.longitude
-  //       }
-  //   })
-  // }
+  function rating(val){
+    let temp = val;
+    let mark = 0;
+    if ( temp < 5) {
+      console.log("meh");
+    } else {
+      console.log("Lekker weertje");
+      let p = document.createElement("b");
+      let textNode = document.createTextNode("Prima motor weer!");
+      p.appendChild(textNode);
+      article.appendChild(p);
+    }
+  }
 
-  //fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/60e53b3010cd5224c2ec5d685d4b320b/${hfdhgd},4.880913`)
+
   
