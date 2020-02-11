acf4016062d636eb75ae314c2c90bbf8bdad7a0e
LarsBreuren <larsbreuren@gmail.com>
2020-02-11 19:32:02 +0100

    Added loop for multi locations

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,61 +1,73 @@
-const locaties = {
-  mijdrecht: "52.2050221,4.880913",
-  amsterdam: "52.3702157,4.8951679",
+initialData();
+
+function initialData() {
+
+  const locaties = {
+    mijdrecht: "52.2050221,4.880913",
+    amsterdam: "52.3702157,4.8951679"
   }
 
-  function buildUrl(locatie){
-    let endpoint = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'
-    let key =  "60e53b3010cd5224c2ec5d685d4b320b/"
-    let end = "?units=si"
-    return endpoint + key + locatie + end
-  }    
-
-  for (locatie in locaties) {
-    if (locaties.hasOwnProperty(locatie)) {
-      buildUrl(locatie, locaties[locatie])
-   }
+  for (const property in locaties){
+        let section = document.getElementById("places");
+        let article = document.createElement("article");
+        let ul = document.createElement("ul");
+        section.appendChild(article);
+        fetchData(`${locaties[property]}`, `${property}`, article );
   }
 
-   fetch(buildUrl(locaties.mijdrecht))
-  .then((response) => {
-    return response.json();
-  })
-  
-  .then((myJson) => {
-    let obj = {
-      Temperatuur: myJson.currently.temperature,
-      Vochtigheid: myJson.currently.humidity,
-      Wolken: myJson.currently.cloudCover,
-      windSpeed: myJson.currently.windSpeed}
+}
+
+function fetchData(loc,name, article){
+  fetch(buildUrl(loc))
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((myJson) => {
+      let obj = {
+        locatie: name,
+        Temperatuur: myJson.currently.temperature,
+        Vochtigheid: myJson.currently.humidity,
+        Wolken: myJson.currently.cloudCover,
+        Windsnelheid: myJson.currently.windSpeed,
+        Zicht: myJson.currently.visibility
+      }
 
       let temp = obj.Temperatuur;
-      let article = document.getElementById("article");
 
       for (let [key, value] of Object.entries(obj)) {
-        console.log(`${key}: ${value}`);
 
-      let p = document.createElement("p");
-      let textNode = document.createTextNode(`${key}: ${value}`);
-  
-      p.appendChild(textNode);
-      article.appendChild(p);
+        let li = document.createElement("li");
+
+        let textNode = document.createTextNode(`${key}: ${value}`);
+        
+        li.appendChild(textNode);
+        article.appendChild(li);
       }
-      rating(temp);
-  });   
+      rateWeather(temp);
+    });
+}
 
-  function rating(val){
-    let temp = val;
-    let mark = 0;
-    if ( temp < 5) {
-      console.log("meh");
-    } else {
-      console.log("Lekker weertje");
-      let p = document.createElement("b");
-      let textNode = document.createTextNode("Prima motor weer!");
-      p.appendChild(textNode);
-      article.appendChild(p);
-    }
+function buildUrl(locatie) {
+  let endpoint = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'
+  let key = "60e53b3010cd5224c2ec5d685d4b320b/"
+  let end = "?units=si"
+  return endpoint + key + locatie + end
+}
+
+function rateWeather(current) {
+  let temp = current;
+  let mark = 0;
+  if (temp < 5) {
+    console.log("meh");
+  } else {
+
+    let advice = document.getElementById("advice");
+    let p = document.createElement("b");
+    let textNode = document.createTextNode("Prima motor weer!");
+
+
+    p.appendChild(textNode);
+    advice.appendChild(p);
   }
-
-
-  
+}
