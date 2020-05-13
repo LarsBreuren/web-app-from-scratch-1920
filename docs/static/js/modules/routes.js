8ea553178d8867b97aa3cd1ba07a9d9f42268243
LarsBreuren <larsbreuren@gmail.com>
2020-05-13 20:40:29 +0200

    refactory of router

--- docs/static/js/modules/routes.js
+++ docs/static/js/modules/routes.js
@@ -1,20 +1,20 @@
 
 export function router(){
-routie({ //Use the # in the url to check what location is asked for.
+  routie({ //Use the # in the url to check what location is asked for.
     ':id' :locatie => {
       updateUI(locatie); // run updateUi with the given location
     },
   });
 }
 
-  export function updateUI(route) {
-    const sections = document.querySelectorAll('section');
-  
-    sections.forEach(section => {
-      section.classList.remove('active'); //Remove all locations
-      section.classList.remove('checkDetails');
-    });
-    let activeSection = document.querySelector(`[data-route=${route}]`); //Check what location matches the current one
-    activeSection.classList.add('active'); //Show only the chosen location
-    activeSection.classList.add('checkDetails');
+export function updateUI(route) {
+  const sections = document.querySelectorAll('section');
+
+  sections.forEach(section => {
+    section.classList.remove('active'); //Remove all locations
+    section.classList.remove('checkDetails');
+  });
+  let activeSection = document.querySelector(`[data-route=${route}]`); //Check what location matches the current one
+  activeSection.classList.add('active'); //Show only the chosen location
+  activeSection.classList.add('checkDetails');
 }
