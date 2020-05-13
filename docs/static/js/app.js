26b827c21cd358a4c571ade4bf463aacd36c6a76
LarsBreuren <larsbreuren@gmail.com>
2020-05-13 17:44:03 +0200

    inputData fixed

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -1,5 +1,8 @@
 import { initialData } from './modules/initialData.js';
 import { router } from './modules/routes.js';
+import { inputData } from './modules/inputData.js';
+
+document.getElementById("button").addEventListener("click", inputData); //Listen to a click on add location button
 
 initialData(); //Run initialData to retrieve weather data from chosen object
 router(); //Handle routes
--- docs/static/js/modules/inputData.js
+++ docs/static/js/modules/inputData.js
@@ -1,6 +1,5 @@
 import { fetchData } from './initialData.js';
 
-document.getElementById("button").addEventListener("click", inputData); //Listen to a click on add location button
 
 export function inputData(){ //Add the location to the website
     let latitude = document.getElementById('Latitude').value
